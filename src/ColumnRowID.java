f8dce5c473bda0d1480ba79478fddc1c2683b72c
root <root@archlinux.Home>
2017-10-14 06:15:20 -0600

    Add ColumnRowID.java

---
+++ src/ColumnRowID.java
@@ -0,0 +1,10 @@
+public class ColumnRowID{
+	public int columnNumber;
+	public int rowNumber;
+	
+	public ColumnRowID(int columnNumber, int rowNumber) {
+		this.columnNumber = columnNumber;
+		this.rowNumber = rowNumber;
+	}
+	
+}
